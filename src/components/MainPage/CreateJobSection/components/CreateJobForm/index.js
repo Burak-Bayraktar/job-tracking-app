7cefa61fddbaeb1cb777757d7041f88724e66341
Burak Bayraktar <byrktrhmx@gmail.com>
2022-04-10 16:32:30 +0300

    Fix for build

--- src/components/MainPage/CreateJobSection/components/CreateJobForm/index.js
+++ src/components/MainPage/CreateJobSection/components/CreateJobForm/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import { useJob } from "../../../../../contexts/JobContext";
 import Button from "../../../../shared/FormInput/Button";
